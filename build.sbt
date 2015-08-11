df778ebeada06e6abe564d638d978d4da6413a66
liping <1522801629liping@sina.com>
2015-08-11 14:10:33 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "world"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.4.1"
+
+resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
---
+++ src/main/scala/NMF/runNMF.scala
@@ -0,0 +1,80 @@
+package NMF
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.graphx.util.GraphGenerators
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx._
+import java.io.PrintWriter
+import java.util.concurrent.TimeUnit
+import java.io.File
+import java.io.IOException
+
+object runNMF {
+  def deleteDir(dir: File) {
+    if (dir.isDirectory()) {
+      val children: Array[String] = dir.list()
+      for (child <- children) {
+        deleteDir(new File(dir, child))
+      }
+    }
+    dir.delete()
+  }
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("GraphNMFApp")
+    val sc = new SparkContext(conf)
+
+    def LoaderEdgeFile(
+                        edgesFile: String,
+                        edgminPartitions: Int = 2):Graph[Int, Double] =
+    {
+      val edgesfile =sc.textFile(edgesFile, edgminPartitions)
+      val edges: RDD[Edge[Double]] = edgesfile.map(
+        line => {
+            val edgesfields = line.trim().split("\\s+", 3)
+            Edge(edgesfields(0).toLong, edgesfields(1).toLong, edgesfields(2).toDouble)
+
+      }
+      )
+      val defaultVD = 0
+      val nullVert: RDD[(VertexId, Int)] = null
+      Graph.fromEdges(edges, defaultVD, StorageLevel.MEMORY_AND_DISK,
+        StorageLevel.MEMORY_AND_DISK).partitionBy(PartitionStrategy.RandomVertexCut)
+    }
+    //val edgesFile: String = args(0)
+    val edgminPartitions: Int = 1
+    val reducedDim: Int = 20
+//    val maxIteration: Int = 1
+
+    val theta: Double = 0.01
+    val lambda: Double = 0.1
+//    val output: String = args(1)
+    val withZeroItems: Int = 0
+    val Seq(edgesFile, output, maxIter) = args.toSeq
+    val maxIteration = maxIter.toInt
+    //val loader = new GraphLoader(sc)
+    val graph: Graph[Int, Double] = LoaderEdgeFile(edgesFile, edgminPartitions)
+
+    val startTime = System.nanoTime()
+    val result = withZeroItems match {
+      case 0 => NMFImp.run(graph, maxIteration, theta, lambda, reducedDim)
+      case 1 => NMFImp.runWithZero(graph, maxIteration, theta, lambda, reducedDim)
+    }
+    val estimatedTime = System.nanoTime() - startTime;
+    println("algorithm running time:\t" + TimeUnit.NANOSECONDS.toSeconds(estimatedTime))
+
+    deleteDir(new File(output))
+    result.vertices.sortBy(_._1, true).saveAsTextFile(output)
+    //val a1 = result.vertices.sortBy(_._1,true).map(v=>(v._1,v._2._1))
+    //val a2 = result.vertices.sortBy(_._1,true).map(v=>(v._1,v._2._2))
+    // a1.saveAsTextFile(output)
+    //a2.saveAsTextFile(output+"0")
+
+    //    val out = new PrintWriter(output)
+    //    out.println(result.vertices.collect().sortWith((VD1, VD2) => VD1._1 < VD2._1).mkString("\n"))
+    //    out.println(result.vertices.collect().mkString("\n"))
+    //    out.close()
+    sc.stop()
+  }
+}
