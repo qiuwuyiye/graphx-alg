69416be160dde501475a6f9bcaf1afed1f16d688
yxh <yanxiaohui@ict.ac.cn>
2015-08-17 11:42:09 +0800

    code reivew

--- src/main/scala/NMF/NMFImp.scala
+++ src/main/scala/NMF/NMFImp.scala
@@ -1,6 +1,5 @@
 package NMF
 
-import org.apache.spark
 import org.apache.spark.graphx._
 //import org.apache.spark.mllib.linalg.Vector
 import scala.reflect.ClassTag
@@ -12,7 +11,7 @@ import org.apache.spark.util.Vector
  * Graph NMF algorithm implementation.
  *
  * Implementation Idea based on Pregel
- *
+ *      D = WH
  * During the iteration, design the direction of the message propagating
  * The initial iteration number is 1
  * When iteration number is odd, forward propagate
@@ -24,10 +23,10 @@ import org.apache.spark.util.Vector
  * the other is Vector H
  *
  * update Rule:
- * W_i <- (1-learnRate*reg)*W_i + learnRate sigma{ (d_ij - W_i*H_j) * H_j }
- * H_j <- (1-learnRate*reg)*H_j + learnRate sigma{ (d_ij - W_i*H_j) * W_i }
+ * W_i <- (1-learn_rate*reg)*W_i + learn_rate sigma{ (d_ij - W_i*H_j) * H_j }
+ * H_j <- (1-learn_rate*reg)*H_j + learn_rate sigma{ (d_ij - W_i*H_j) * W_i }
  *
- * `learnRate` is the learning rate
+ * `learn_rate` is the learning rate
  * `reg` is the regularization coefficient
  *
  */
@@ -41,98 +40,104 @@ object NMFImp {
    * @tparam VD the original vertex attribute (not used)
    *
    * @param graph the graph on which to run NMF, the edge attribute must be Double
-   * @param maxIterations the max iteration
-   * @param learnRate the learn rate
+   * @param maxIter the max iteration
+   * @param learn_rate the learn rate
    * @param reg the normalization item
-   * @param reducedDim the reduced Dimension in NMF algorithm
+   * @param K the reduced Dimension in NMF algorithm
    *
    * @return the graph containing the two Vectors which is Vector W and Vector H
    *         and edge attributes containing the edge weight.
    *
    */
   def run[VD: ClassTag](graph: Graph[VD, Double],
-    maxIterations: Int = 10,
-    learnRate: Double = 0.01,
+    maxIter: Int = 10,
+    learn_rate: Double = 0.01,
     reg: Double = 0.1,
-    reducedDim: Int = 50) = {
+    K: Int = 50) = {
 
-    /** Update graph vertex attribute function.
+    /**
+     * Update graph vertex attribute function.
      *
      * @param id  Vertex id
      * @param attri  Vertex attribute
      * @param msgSum The combined message sum from other vertex
      * @return A new vector and it's the vertex's new attribute
      */
-    def VertexProgram(id: VertexId, attri: Vector, msgSum: Vector): Vector = {
-      val scale = 1 - learnRate * reg
-      val intercept = learnRate * msgSum
+    def updateVertex(id: VertexId, attri: Vector, msgSum: Vector): Vector = {
+      val scale = 1 - learn_rate * reg
+      val intercept = learn_rate * msgSum
       val newV: Vector = scale * attri + intercept
       // set the negative value to zero
       Vector(newV.elements.map(math.max(_, 0)))
     }
 
-    /**Send messages to the source vertex with the opposite edge direction
+    /**
+     * Send messages to the source vertex with the opposite edge direction
      *
      * @param triplet [vertex attribute, edge attribute, message]
      */
-    def forwardSendMessage(triplet: EdgeContext[Vector, Double, Vector]) {
+    def sendDeltaOfW(triplet: EdgeContext[Vector, Double, Vector]) {
       triplet.sendToSrc((triplet.attr - triplet.srcAttr.dot(triplet.dstAttr)) * triplet.dstAttr)
     }
 
-    /**Send messages to the destination vertex with edge direction
+    /**
+     * Send messages to the destination vertex with edge direction
      *
      * @param triplet [vertex attribute, edge attribute, message]
      */
-    def backSendMessage(triplet: EdgeContext[Vector, Double, Vector]){
+    def sendDeltaOfH(triplet: EdgeContext[Vector, Double, Vector]) {
       triplet.sendToDst((triplet.attr - triplet.srcAttr.dot(triplet.dstAttr)) * triplet.srcAttr)
     }
 
-    /** Combine the two messages and return the sum of them.
-     *
+    /**
+     * Combine the two messages and return the sum of them.
      * @param a A vector
      * @param b A vector
      * @return  Sum of a and b
      */
     def messageCombiner(a: Vector, b: Vector): Vector = a + b
-    /** Initiate each Vertex's vector W and vector H whose dimension is reducedDim
-      *
-      */
+
+    // Initiate each Vertex's vector W and vector H whose dimension is K
     var curGraph: Graph[Vector, Double] = graph.mapVertices { (vid, vdata) =>
-      Vector(Array.fill(reducedDim)(Random.nextDouble))
+      Vector(Array.fill(K)(Random.nextDouble))
     }.cache()
-    var messages = curGraph.aggregateMessages(forwardSendMessage, messageCombiner)
-    var activeMessages = messages.count()
-    var curIteration: Int = 1
+
+
+    var delta: VertexRDD[Vector] = curGraph.aggregateMessages(sendDeltaOfW, messageCombiner).cache()
+
+    // number of vertexes updated
+    var n_update = delta.count()
+    var iter: Int = 1
     var prevGraph: Graph[Vector, Double] = null
-    while (activeMessages > 0 && (curIteration - 1) / 2 < maxIterations) {
-      if ((curIteration - 1) % 2 == 0) {
+    while (n_update > 0 && ((iter - 1) / 2 < maxIter)) {
+      if ((iter - 1) % 2 == 0) {
         // Forward messages passing
-        val newVerts: VertexRDD[Vector] = curGraph.vertices
-          .innerJoin(messages)(VertexProgram).cache()
+        val new_W: VertexRDD[Vector] = curGraph.vertices
+          .innerJoin(delta)(updateVertex).cache()
         prevGraph = curGraph
-        curGraph = curGraph.outerJoinVertices(newVerts) { (vid, old, newOpt) =>
+        curGraph = curGraph.outerJoinVertices(new_W) { (vid, old, newOpt) =>
           newOpt.getOrElse(old)
         }
         curGraph.cache()
-        val oldMessages = messages
-        messages = curGraph.aggregateMessages(backSendMessage, messageCombiner).cache()
-        oldMessages.unpersist(blocking = false)
-        newVerts.unpersist(blocking = false)
+        val old_delta = delta
+        delta = curGraph.aggregateMessages(sendDeltaOfH, messageCombiner).cache()
+        old_delta.unpersist(blocking = false)
+        new_W.unpersist(blocking = false)
       } else {
         //Backward message passing
-        val newVerts = curGraph.vertices.innerJoin(messages)(VertexProgram).cache()
+        val new_H = curGraph.vertices.innerJoin(delta)(updateVertex).cache()
         prevGraph = curGraph
-        curGraph = curGraph.outerJoinVertices(newVerts) { (vid, old, newOpt) => newOpt.getOrElse(old) }
+        curGraph = curGraph.outerJoinVertices(new_H) { (vid, old, newOpt) => newOpt.getOrElse(old) }
         curGraph.cache()
-        val oldMessages = messages
-        messages = curGraph.aggregateMessages(forwardSendMessage, messageCombiner).cache()
-        oldMessages.unpersist(blocking = false)
-        newVerts.unpersist(blocking = false)
+        val old_delta = delta
+        delta = curGraph.aggregateMessages(sendDeltaOfW, messageCombiner).cache()
+        old_delta.unpersist(blocking = false)
+        new_H.unpersist(blocking = false)
       }
-      activeMessages = messages.count()
+      n_update = delta.count()
       prevGraph.unpersistVertices(blocking = false)
       prevGraph.edges.unpersist(blocking = false)
-      curIteration += 1
+      iter += 1
     }
     curGraph
   }
--- src/main/scala/NMF/runNMF.scala
+++ src/main/scala/NMF/runNMF.scala
@@ -7,93 +7,91 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx._
 import java.util.concurrent.TimeUnit
 
+case class Conf(
+  edge_pt: String = "hdfs://bda00:8020/user/liping/edges.txt",
+  output_pt: String = "hdfs://bda00:8020/user/liping/NMFResult",
+  n_partition: Int = 2,
+  K: Int = 20,
+  learn_rate: Double = 0.01,
+  reg: Double = 0.1,
+  maxIter: Int = 40
+  )
 
 object runNMF {
   val conf = new SparkConf().setAppName("NMF")
   val sc = new SparkContext(conf)
-  /** This function loads an edge file and then return a graph, the format is like:(vertexId vertexId edgeAttr)
-    *
-    * @param edgesFile
-    * @param edgesMinPartitions
-    * @return A Graph
-    */
-  def LoaderEdgeFile(edgesFile: String,
-    edgesMinPartitions: Int = 2): Graph[Int, Double] = {
-    val edgesfile = sc.textFile(edgesFile, edgesMinPartitions)
-    val edges: RDD[Edge[Double]] = edgesfile.map(
-      line => {
-        val edgesfields = line.trim().split("\\s+", 3)
-        Edge(edgesfields(0).toLong, edgesfields(1).toLong, edgesfields(2).toDouble)
 
-      }
-    )
-    val defaultVD = 0
-    val nullVert: RDD[(VertexId, Int)] = null
-    Graph.fromEdges(edges, defaultVD, StorageLevel.MEMORY_AND_DISK,
+  /**
+   * Create a graph by loading edges from a file
+   *
+   * @param edge_pt   each line format: (vertexId vertexId edgeAttr)
+   * @param n_partition  minimum partition size of edges
+   */
+  def loadEdgeFile(edge_pt: String,
+    n_partition: Int = 2): Graph[Int, Double] = {
+    val edges: RDD[Edge[Double]] = sc.textFile(edge_pt, n_partition).map { line =>
+      val Array(v1, v2, e) = line.trim().split("\\s+", 3)
+      Edge(v1.toLong, v2.toLong, e.toDouble)
+    }
+
+    Graph.fromEdges(edges, 0, StorageLevel.MEMORY_AND_DISK,
       StorageLevel.MEMORY_AND_DISK).partitionBy(PartitionStrategy.RandomVertexCut)
   }
 
-  /**Main function
-   *
-   * @param args Read input edge file. output file and the iter_num
-   */
-  def main(args: Array[String]) {
-    /**
-     * para decomposition
-     */
 
-      val parser = new scopt.OptionParser[ParaConfig]("runNMF"){
+  def main(args: Array[String]) {
+    // para decomposition
+    val parser = new scopt.OptionParser[Conf]("runNMF") {
       head("NMF", "1.0")
-      opt[String]("edgesFile") optional() action { (x, c) =>
-      c.copy(edgesFile = x)
-      } text ("edgesFile is the input file that includes the graph information")
-      opt[String]("output") optional() action { (x, c) =>
-        c.copy(output = x)
-      } text ("output is the output file that stores two matrix W and H")
-      opt[Int]("reducedDim") optional() action { (x, c) =>
-        c.copy(reducedDim = x)
-      } validate { x => if (x > 0) success else failure("Option --reducedDim must >0")
+      opt[String]("edge_pt") optional() action { (x, c) =>
+        c.copy(edge_pt = x)
+      } text ("edge_pt is the input file that includes the graph information")
+      opt[String]("output_pt") optional() action { (x, c) =>
+        c.copy(output_pt = x)
+      } text ("output_pt is the output_pt file that stores two matrix W and H")
+      opt[Int]("K") optional() action { (x, c) =>
+        c.copy(K = x)
+      } validate { x => if (x > 0) success else failure("Option --K must >0")
       } text ("reduceDim is the factorized dimension which is known as K")
-      opt[Int]("maxIteration") optional() action { (x, c) =>
-        c.copy(maxIteration = x)
-      } validate { x => if (x > 0) success else failure("Option --maxIteration must >0")
+      opt[Int]("maxIter") optional() action { (x, c) =>
+        c.copy(maxIter = x)
+      } validate { x => if (x > 0) success else failure("Option --maxIter must >0")
       } text ("maxIteraton is the max Iteration count of this factorization program")
-      opt[Int]("edgesMinPartition") optional() action { (x, c) =>
-        c.copy(edgesMinPartition = x)
-      } validate { x => if (x > 0) success else failure("Option --edgesMinPartition must >0")
-      } text ("edgesMinPartition is the min number of RDD's split parts, default is 2")
+      opt[Int]("n_partition") optional() action { (x, c) =>
+        c.copy(n_partition = x)
+      } validate { x => if (x > 0) success else failure("Option --n_partition must >0")
+      } text ("n_partition is the min number of RDD's split parts, default is 2")
       opt[Double]("reg") optional() action { (x, c) =>
         c.copy(reg = x)
       } validate { x => if (x > 0.0) success else failure("Option --reg must >0.0")
       } text ("reg is the regularization part, default if 0.1")
-      opt[Double]("learnRate") optional() action { (x, c) =>
-        c.copy(learnRate = x)
-      } validate { x => if (x > 0.00) success else failure("Option --learnRate must >0.00")
-      } text("learning rate, default is 0.01")
+      opt[Double]("learn_rate") optional() action { (x, c) =>
+        c.copy(learn_rate = x)
+      } validate { x => if (x > 0.00) success else failure("Option --learn_rate must >0.00")
+      } text ("learning rate, default is 0.01")
     }
-    val para: ParaConfig = parser.parse(args, ParaConfig()).get
-    val edgesMinPartition = para.edgesMinPartition
-    val reducedDim = para.reducedDim
-    val learnRate = para.learnRate
+
+    val para: Conf = parser.parse(args, Conf()).get
+    val n_partition = para.n_partition
+    val K = para.K
+    val learn_rate = para.learn_rate
     val reg = para.reg
-    val edgesFile = para.edgesFile
-    val output = para.output
-    val maxIteration = para.maxIteration
-    val graph: Graph[Int, Double] = LoaderEdgeFile(edgesFile, edgesMinPartition)
+    val edge_pt = para.edge_pt
+    val output_pt = para.output_pt
+    val maxIter = para.maxIter
+    val graph: Graph[Int, Double] = loadEdgeFile(edge_pt, n_partition)
 
     val startTime = System.nanoTime()
-    /**
-     * Eecute the matrix factorization Method
-     */
-    val result = NMFImp.run(graph, maxIteration, learnRate, reg, reducedDim)
+
+    //Execute the matrix factorization Method
+    val result = NMFImp.run(graph, maxIter, learn_rate, reg, K)
     val estimatedTime = System.nanoTime() - startTime;
     println("algorithm running time:\t" + TimeUnit.NANOSECONDS.toSeconds(estimatedTime))
 
-    /**
-     * save the result matrix to hdfs
-     */
-    result.vertices.filter(_._1 < 0).sortBy(_._1, false).saveAsTextFile(output + "H")
-    result.vertices.filter(_._1 > 0).sortBy(_._1, true).saveAsTextFile(output + "W")
+
+    // save the result matrix to hdfs
+    result.vertices.filter(_._1 < 0).sortBy(_._1, false).saveAsTextFile(output_pt + "H")
+    result.vertices.filter(_._1 > 0).sortBy(_._1, true).saveAsTextFile(output_pt + "W")
     sc.stop()
   }
 }
